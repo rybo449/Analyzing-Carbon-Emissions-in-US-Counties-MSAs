a6e2fd5d16c21852a5c14bb288a481b35686fca1
rybo449 <sriniketh.nitk@gmail.com>
2014-11-23 17:58:10 -0500

    removed

--- Assignment 4/2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import sys
-
-df = pd.read_csv(sys.argv[1])
-#df['population densty'] = df['TOTAL POP IN MSA']/df['TOTAL LAND IN MSA']
-df1 = df[['POP DENSITY', 'CO2/CAPITA']]
-df1.plot(kind = 'scatter')
-plt.show()
